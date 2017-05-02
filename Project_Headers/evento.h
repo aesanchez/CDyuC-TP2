a69efd5b6f77c7b9504b1865a2a546cbc038f92b
Matias Pan <matias.pan26@gmail.com>
2017-05-02 06:42:28 -0700

    libreria para manejo de eventos

---
+++ Project_Headers/evento.h
@@ -0,0 +1,8 @@
+#ifndef EVENTO_H_
+#define EVENTO_H_
+
+void push_tecla(unsigned char);
+unsigned char pop_tecla(void);
+unsigned char cola_vacia(void);
+
+#endif /* EVENTO_H_ */
---
+++ Sources/evento.c
@@ -0,0 +1,16 @@
+unsigned char tecla;
+unsigned char vacio=1;
+
+void push_tecla(unsigned char t){
+	tecla = t;
+	vacio=0;
+}
+
+unsigned char pop_tecla(void){
+	vacio=1;
+	return tecla;
+}
+
+unsigned char cola_vacia(void){
+	return vacio;
+}
